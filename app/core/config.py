6ccd0f523dd0f8d0a5424813e3d400fb6388b8fd
sigma31 <pranetsh@bu.edu>
2025-04-16 22:26:22 -0400

    Used Pydantic to be able to load environment variables from the .env file

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    openai_api_key: str
+    database_url: str = "sqlite:///./graded_results.db"  # Default
+
+    class Config:
+        env_file = ".env"  # Automatically load the class key from .env file
+
+
+settings = Settings()
